647d3100cbf4f170e23b5f0dc3b72af922de7f27
HOME <kraynoff.i@yandex.ru>
2023-12-01 23:28:32 +0300

    fix and replace

---
+++ test.py
@@ -0,0 +1,23 @@
+import parmed.unit as u
+from numpy import AxisError
+
+u.asd
+u.arcseconds
+class AG:
+    fs=2
+    @property
+    def foo(self,j):
+        return 2*j
+    def goo(self):
+        self.h
+        return 2
+
+
+a= AG()
+a.fs()
+a.hg
+a.foo
+a.goo
+simulation=1
+simulation.context.getState
+j
