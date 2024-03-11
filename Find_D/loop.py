f4b7644925d5b2961ce6c20d0fb14bb4cc5fffc4
WSL_Home <WSL_Home_email>
2024-03-11 23:00:39 +0300

    fix loop

--- Find_D/loop.py
+++ Find_D/loop.py
@@ -1,20 +1,27 @@
-import os
 from pathlib import Path
-
+import os
+import openmm
 import openmm.app as openmm_app
 import pandas as pd
-import tools.simulator as simulator
-from tqdm import tqdm as _tqdm
+from tools import base, simulator
 
 
 def loop():
-    for path in os.listdir('./Find_D/systems'):
-        top_path = Path(f'./Find_D/systems/{path}') / Path('top.pdb')
-        system_path = Path(f'./Find_D/systems/{path}') / Path('system.xml')
-        result_dir = Path(f'./Find_D/results/{path}')
+    for path in os.listdir('Find_D/systems'):
+        sim_path = Path(f'Find_D/systems/{path}')
+        substance, x, rho = path.split('_')
+        result_dir = Path(f'Find_D/results/{sim_path.stem}')
         result_dir.mkdir(parents=True, exist_ok=True)
 
-        substance, x, rho = path.split('_')
         print('#' * 30, f'{substance:10}: {int(x):<3}')
-
         for T in [15, 20, 25, 30, 40]:
+            print('#' * 20, 'Tempetature: ', T)
+            sim = simulator.Simulation(T=T)
+            sim.load(sim_path)
+            sim.equilibrate(20)
+            sim.simulate(1000)
+            df: pd.DataFrame = sim.get_data()
+            df.to_csv(f'{result_dir}/{T}.csv')
+
+
+loop()
